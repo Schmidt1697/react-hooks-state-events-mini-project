aeef4ace4b733dbf432e485c99ba90995f9878da
Anna Schmidt <marshall1697@gmail.com>
2022-09-26 11:52:45 -0600

    finished task deliverables except for delete button

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,9 +1,20 @@
 import React from "react";
+import Task from "./Task";
 
-function TaskList() {
+function TaskList({ tasks }) {
+  console.log(tasks)
+  
+  const taskList = tasks.map((task, index) =>{
+    return(
+      <Task key={index} {...task}/>
+    )
+  })
+
+  console.log(taskList)
+  
   return (
     <div className="tasks">
-      {/* display a list of tasks using Task component */}
+      {taskList}
     </div>
   );
 }
